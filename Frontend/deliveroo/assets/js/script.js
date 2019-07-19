fe409868e49bf7863eb9a1e313e8132971a51dd2
Marine Bottone <marinebottone@air-de-marine.lan>
2019-07-19 16:54:40 +0200

    finishing deliveroo (head/foot)

--- Frontend/deliveroo/assets/js/script.js
+++ Frontend/deliveroo/assets/js/script.js
@@ -1,8 +1,19 @@
-/* const $ = document
+const $ = document
+const container = $.querySelectorAll(".container-fluid")
+console.log(container)
 
 window.addEventListener("resize", () => {
     if (window.innerWidth < 1200) {
-        const container = $.querySelector(".container")
-        container.classList.add("container100")
+        for (let i = 0; i < container.length; i++) {
+            console.log("< 1200", container[i].classList)
+            container[i].classList.remove("container")
+        }
+
+    } else {
+        for (let i = 0; i < container.length; i++) {
+            console.log("> 1200", container[i].classList)
+            container[i].classList.add("container")
+        }
+
     }
-}) */
+})  
