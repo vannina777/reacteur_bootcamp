37c15c96b56d792ff3c460d884e1e7953055e053
Marine Bottone <marinebottone@air-de-marine.lan>
2019-07-19 09:05:28 +0200

    work on deliveroo copycat

---
+++ Frontend/deliveroo/assets/js/script.js
@@ -0,0 +1,8 @@
+/* const $ = document
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth < 1200) {
+        const container = $.querySelector(".container")
+        container.classList.add("container100")
+    }
+}) */
