46fc1150c81503442b92582b346407fa44805698
Theau Poulat <theau.poulat@gmail.com>
2019-07-30 16:40:01 +0200

    refactored with hooks

--- React/counter/src/App.js
+++ React/counter/src/App.js
@@ -1,120 +1,149 @@
-import React from 'react';
+import React, { useState } from "react";
 
+const App = () => {
+  const [counterA, setCounterA] = useState(0);
+  const [counterB, setCounterB] = useState(0);
+  const [counterC, setCounterC] = useState(0);
 
-class App extends React.Component {
+  let total = counterA + counterB + counterC;
 
-  state = { counterA: 0, counterB: 0, counterC: 0, total: 0 }
+  let buttonAddA = (
+    <button
+      onClick={() => {
+        setCounterA(counterA + 1);
+        total += 1;
+      }}
+    >
+      {" "}
+      +
+    </button>
+  );
 
-  render = () => {
+  if (counterA >= 10) {
+    buttonAddA = null;
+  }
 
-    // button 1
+  let buttonSubA = (
+    <button
+      onClick={() => {
+        setCounterB(counterA - 1);
+      }}
+    >
+      {" "}
+      -
+    </button>
+  );
 
-    let buttonAddA = <button onClick={() => {
-      this.setState({ counterA: this.state.counterA + 1, total: this.state.total + 1 })
-    }} > +</button >
+  if (counterA <= 0) {
+    buttonSubA = null;
+  }
 
-    if (this.state.counterA >= 10) {
-      buttonAddA = null
-    }
+  //buttons B
 
-    let buttonSubA = <button onClick={() => {
-      this.setState({ counterA: this.state.counterA - 1, total: this.state.total - 1 })
-    }} > -</button >
+  let buttonAddB = (
+    <button
+      onClick={() => {
+        setCounterB(counterB + 1);
+      }}
+    >
+      {" "}
+      +
+    </button>
+  );
 
-    if (this.state.counterA <= 0) {
-      buttonSubA = null
-    }
+  if (counterB >= 10) {
+    buttonAddB = null;
+  }
 
+  let buttonSubB = (
+    <button
+      onClick={() => {
+        setCounterB(counterB - 1);
+      }}
+    >
+      {" "}
+      -
+    </button>
+  );
 
+  if (counterB <= 0) {
+    buttonSubB = null;
+  }
 
-    //buttons B
+  // buttons C
 
-    let buttonAddB = <button onClick={() => {
-      this.setState({ counterB: this.state.counterB + 1, total: this.state.total + 1 })
-    }} > +</button >
+  let buttonAddC = (
+    <button
+      onClick={() => {
+        setCounterC(counterC + 1);
+      }}
+    >
+      {" "}
+      +
+    </button>
+  );
 
-    if (this.state.counterB >= 10) {
-      buttonAddB = null
-    }
+  if (counterC >= 10) {
+    buttonAddC = null;
+  }
 
-    let buttonSubB = <button onClick={() => {
-      this.setState({ counterB: this.state.counterB - 1, total: this.state.total - 1 })
-    }} > -</button >
+  let buttonSubC = (
+    <button
+      onClick={() => {
+        setCounterC(counterC - 1);
+      }}
+    >
+      {" "}
+      -
+    </button>
+  );
 
-    if (this.state.counterB <= 0) {
-      buttonSubB = null
-    }
+  if (counterC <= 0) {
+    buttonSubC = null;
+  }
 
-    // buttons C
+  // block at 18 -- not good, need to refactor
 
-    let buttonAddC = <button onClick={() => {
-      this.setState({ counterC: this.state.counterC + 1, total: this.state.total + 1 })
-    }} > +</button >
+  if (total >= 18) {
+    buttonAddA = null;
+    buttonAddB = null;
+    buttonAddC = null;
+  }
 
-    if (this.state.counterC >= 10) {
-      buttonAddC = null
-    }
+  // declare a boolean with the button JSX as an argument (that is true ) so if left is true -> passes the element to the right
 
-    let buttonSubC = <button onClick={() => {
-      this.setState({ counterC: this.state.counterC - 1, total: this.state.total - 1 })
-    }} > -</button >
+  return (
+    <div className="container">
+      <h1> Etape 5 </h1>
+      <div className="Counter">
+        <div>
+          {buttonSubA}
 
-    if (this.state.counterC <= 0) {
-      buttonSubC = null
-    }
+          <span className="Counter--screen"> {counterA} </span>
 
-
-    // block at 18 -- not good, need to refactor
-
-    if (this.state.total >= 18) {
-      buttonAddA = null;
-      buttonAddB = null;
-      buttonAddC = null;
-    }
-
-    // declare a boolean with the button JSX as an argument (that is true ) so if left is true -> passes the element to the right
-
-
-
-    return (
-      <div className="container" >
-
-
-        <h1> Etape 5 {this.props.name}</h1>
-        <div className="Counter">
-          <div>
-            {buttonSubA}
-
-            <span className="Counter--screen"> {this.state.counterA} </span>
-
-            {buttonAddA}
-          </div>
-
-          <div>
-            {buttonSubB}
-
-            <span className="Counter--screen"> {this.state.counterB} </span>
-
-            {buttonAddB}
-          </div>
-
-          <div>
-            {buttonSubC}
-
-            <span className="Counter--screen"> {this.state.counterC} </span>
-
-            {buttonAddC}
-          </div>
-
-          <h1> Total: {this.state.total}</h1>
-
+          {buttonAddA}
         </div>
 
+        <div>
+          {buttonSubB}
+
+          <span className="Counter--screen"> {counterB} </span>
+
+          {buttonAddB}
+        </div>
+
+        <div>
+          {buttonSubC}
+
+          <span className="Counter--screen"> {counterC} </span>
+
+          {buttonAddC}
+        </div>
+
+        <h1> Total: {total}</h1>
       </div>
-
-
-    )
-  }
-}
+    </div>
+  );
+};
 
 export default App;
